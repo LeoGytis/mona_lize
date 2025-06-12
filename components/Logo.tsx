525903818a0edb93a52ad736789849cba850d3f1
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-06-12 13:53:10 +0300

    save

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -9,16 +9,16 @@ const BorderedLogo = () => {
 			to="sectionMenu"
 			smooth={true}
 			duration={500}
-			className="flex border-2 border-white rounded-full cursor-pointer max-lg:scale-75"
+			className="flex border-2 border-white rounded-full cursor-pointer"
 		>
 			<div className="border-black rounded-full border-5">
 				<div className="border-white rounded-full border-5">
-					<div className="p-6 bg-black border-black rounded-full border-3 ">
+					<div className="p-3 md:p-6 bg-black border-black rounded-full border-3 size-24 md:size-40 flex items-center justify-center">
 						<Image
 							src={LogoImage}
 							alt="logo"
-							width={100}
-							height={20}
+							width={140}
+							height={80}
 						/>
 					</div>
 				</div>
--- layout/NavBar.tsx
+++ layout/NavBar.tsx
@@ -27,28 +27,28 @@ export const Navbar = () => {
 
 	return (
 		<nav className="flex items-center justify-between sticky top-0 z-[9999] pt-14 lg:pt-20 bg-background">
-			<div className="absolute left-0 ml-10 max-lg:-left-14 lg:ml-6">
+			<div className="absolute top-4 ml-2 lg:ml-6 z-50">
 				<BorderedLogo />
 			</div>
 
-			<div className="flex items-center justify-end w-full gap-2 py-2 ml-10 bg-black shadow-xl xl:ml-16">
-				<div className="container self-end flex justify-end mx-auto lg:mr-[8dvw]">
+			<div className="relative items-center justify-end w-full gap-2 py-6 md:py-8 bg-black shadow-xl xl:ml-16 ml-8">
+				<div className="absolute top-1/2 -translate-y-1/2 container flex justify-end mx-auto">
 					<NavLink to="sectionMenu">Menu</NavLink>
 					<NavLink to="sectionGallery">Galerija</NavLink>
 					<NavLink to="sectionFoodTruck">Apie mus</NavLink>
 				</div>
-				{user ? (
-					<div className="absolute top-5 right-20 ">
-						<button onClick={signOut} className="btn-primary">
-							Log out
-						</button>
-					</div>
-				) : (
-					<div className="absolute !px-8 top-2 md:top-5 right-20 btn-primary">
-						<a href="/sign-in">Sign in</a>
-					</div>
-				)}
 			</div>
+			{user ? (
+				<div className="absolute top-1 right-20">
+					<button onClick={signOut} className="btn-primary">
+						Log out
+					</button>
+				</div>
+			) : (
+				<div className="absolute !px-8 top-1 md:top-5 right-20 btn-primary">
+					<a href="/sign-in">Sign in</a>
+				</div>
+			)}
 		</nav>
 	);
 };

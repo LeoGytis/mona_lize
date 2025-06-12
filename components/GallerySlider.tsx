6cc25a37d86ee8ac67a222668b367362af5149c6
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-06-12 13:31:48 +0300

    save

---
+++ components/GallerySlider.tsx
@@ -0,0 +1,58 @@
+import {Splide, SplideSlide} from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
+import {AutoScroll} from '@splidejs/splide-extension-auto-scroll';
+import Image from 'next/image';
+import {galleryImages} from '../utils/constants';
+
+interface GalleryImageProps {
+	src: string;
+	alt: string;
+}
+
+const GallerySlider = () => {
+	const splideOptions = {
+		type: 'loop',
+		autoScroll: {
+			pauseOnHover: false,
+			pauseOnFocus: false,
+			rewind: true,
+			speed: 1,
+		},
+		arrows: false,
+		pagination: false,
+		gap: '45px',
+		fixedWidth: '600px',
+		fixedHeight: '400px',
+		breakpoints: {
+			640: {
+				fixedWidth: '300px',
+				fixedHeight: '200px',
+				gap: '20px',
+			},
+		},
+	};
+
+	return (
+		<div className="absolute left-1/2 w-[94dvw] -translate-x-1/2 transform md:w-[98dvw]">
+			<Splide options={splideOptions} extensions={{AutoScroll}}>
+				{galleryImages.map(
+					(image: GalleryImageProps, index: number) => (
+						<SplideSlide key={index}>
+							<div className="relative h-full w-full overflow-hidden rounded-lg">
+								<Image
+									src={image.src}
+									alt={image.alt}
+									fill
+									className="object-cover hover:scale-125 transition-all duration-1500"
+									priority={index < 2}
+								/>
+							</div>
+						</SplideSlide>
+					)
+				)}
+			</Splide>
+		</div>
+	);
+};
+
+export default GallerySlider;
--- layout/sections/SectionGallery.tsx
+++ layout/sections/SectionGallery.tsx
@@ -1,52 +1,12 @@
-import Image from 'next/image';
+import GallerySlider from '@/components/GallerySlider';
 
 const SectionGallery = () => {
-	// Array of image paths from the dropbox folder
-	const images = [
-		{src: '/images/dropbox/581A3582.jpg', alt: 'Gallery image 1'},
-		{src: '/images/dropbox/581A3573.jpg', alt: 'Gallery image 2'},
-		{src: '/images/dropbox/581A3447.jpg', alt: 'Gallery image 3'},
-		{src: '/images/dropbox/581A3439.jpg', alt: 'Gallery image 4'},
-		{src: '/images/dropbox/581A3431.jpg', alt: 'Gallery image 5'},
-		{src: '/images/dropbox/581A3426.jpg', alt: 'Gallery image 6'},
-		{src: '/images/dropbox/581A3006.jpg', alt: 'Gallery image 7'},
-		{src: '/images/dropbox/581A2945.jpg', alt: 'Gallery image 8'},
-		{src: '/images/dropbox/581A2941.jpg', alt: 'Gallery image 9'},
-		{src: '/images/dropbox/581A2853.jpg', alt: 'Gallery image 10'},
-		{src: '/images/dropbox/581A2739.jpg', alt: 'Gallery image 11'},
-		{src: '/images/dropbox/581A2727.jpg', alt: 'Gallery image 12'},
-		{src: '/images/dropbox/581A2720.jpg', alt: 'Gallery image 13'},
-		{src: '/images/dropbox/581A2712.jpg', alt: 'Gallery image 14'},
-		{src: '/images/dropbox/581A2702.jpg', alt: 'Gallery image 15'},
-		{src: '/images/dropbox/581A2591.jpg', alt: 'Gallery image 16'},
-		{src: '/images/dropbox/581A2574.jpg', alt: 'Gallery image 17'},
-		{src: '/images/dropbox/581A2354.jpg', alt: 'Gallery image 18'},
-	];
-
 	return (
 		<section
 			id="sectionGallery"
-			className="flex justify-center py-10 lg:py-32 w-full"
+			className="relative flex items-center justify-center py-10 lg:py-32 w-full h-[200px] md:h-[500px]"
 		>
-			<div className="container mx-auto">
-				<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-					{images.map((image, index) => (
-						<div
-							key={index}
-							className="relative aspect-square overflow-hidden rounded-lg transition-transform duration-300 hover:scale-105"
-						>
-							<Image
-								src={image.src}
-								alt={image.alt}
-								fill
-								className="object-cover"
-								// sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw, 25vw"
-								priority={index < 4} // Load first 4 images immediately
-							/>
-						</div>
-					))}
-				</div>
-			</div>
+			<GallerySlider />
 		</section>
 	);
 };
